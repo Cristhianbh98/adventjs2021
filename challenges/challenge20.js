af67f6de471a47afbbfc8c2bb345194fa7374f4e
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2021-12-23 10:56:38 -0500

    Change solution 20

--- challenges/challenge20.js
+++ challenges/challenge20.js
@@ -1,5 +1,23 @@
 // ¿Una carta de pangramas? ¡QUÉ!
 
 export default function pangram (letter) {
-  return /[a-z]ñ/gi.test(letter)
+  const alphabet = 'abcdefghijklmnñopqrst'.split('')
+  const cured = letter
+    .toLowerCase()
+    .replace('á', 'a')
+    .replace('é', 'e')
+    .replace('í', 'i')
+    .replace('ó', 'o')
+    .replace('ú', 'u')
+
+  let response = true
+
+  for (const alph of alphabet) {
+    if (!cured.includes(alph)) {
+      response = false
+      break
+    }
+  }
+
+  return response
 }
